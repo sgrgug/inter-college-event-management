266a3eb63b75a52a11cd8eeeeab0539b4ca9ac2e
Sagar Gurung <sgrgug@gmail.com>
2023-06-08 19:48:13 +0545

    login and register done

---
+++ app/Models/Organization.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Http\User;
+
+class Organization extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'name', 'description'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- database/migrations/2023_06_08_132757_create_organizations_table.php
+++ database/migrations/2023_06_08_132757_create_organizations_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('organizations', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role')->default('user');
-            $table->rememberToken();
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('organizations');
     }
 };
