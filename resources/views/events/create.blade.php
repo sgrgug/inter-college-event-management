5ad057de495a4c51590e0779c0d9972810a36cc0
Sagar Gurung <sgrgug@gmail.com>
2023-06-11 00:08:30 +0545

    small change

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,1 +1,13 @@
-<h1>create</h1>
+<x-app-layout>
+    <div class="max-w-screen-2xl m-auto p-1">
+        <div class="my-10 px-2 md:px-10 lg:px-32">
+
+            
+            <div class="bg-white shadow-sm">
+                <h1 class="inline-block">Create Event</h1>
+            </div>
+
+        </div>
+    </div>
+    
+</x-app-layout>
